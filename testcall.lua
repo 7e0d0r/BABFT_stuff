d7479653ad44b9ad0d6316e3dd156a19ed66d623
Aidar <148871619+7e0d0r@users.noreply.github.com>
2025-02-05 21:28:52 +0500

    Create testcall.lua

---
+++ testcall.lua
@@ -0,0 +1,15 @@
+local function CreateWindow()
+    print('CreateWindow')
+end
+
+local function CreateButton()
+    print('CreateButton')
+end
+
+local function CreateLabel()
+    print('CreateLabel')
+end
+
+local function CreateToggle()
+    print('CreateToggle')
+end
